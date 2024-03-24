04119c15db85d03c9ef76cdec7f51ec4f898d0c5
DESKTOP-IRMCSGQ\Vin.Young <vinyoungfly@gmail.com>
2024-03-24 22:42:55 +0800

    增加数据库连接池和redis连接池，但数据库连接池不可用，报错

--- account/src/main/java/vip/vinyoung/account/controller/RegisterController.java
+++ account/src/main/java/vip/vinyoung/account/controller/RegisterController.java
@@ -11,13 +11,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vip.vinyoung.account.dao.UserInfoDao;
-import vip.vinyoung.account.params.basic.UserParam;
+import vip.vinyoung.account.params.UserDetailParam;
 import vip.vinyoung.tools.bean.basic.CommonResult;
-import vip.vinyoung.tools.bean.mail.MailBasic;
+import vip.vinyoung.tools.config.valication.group.DefaultGroup;
 import vip.vinyoung.tools.service.Log;
 import vip.vinyoung.tools.service.MailService;
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -37,13 +35,11 @@ public class RegisterController implements Log {
 
     @Operation(summary = "注册账号")
     @PostMapping("/register")
-    public CommonResult search(@RequestBody @Validated UserParam param) {
-        // info("Account registration interface input parameters {}",
-        //     param.getUserName()); // 账号体中绝大部分信息属于敏感信息，不打印入参的其他参数的日志
-        List<String> tos = new ArrayList<>();
-        tos.add(param.getEmail());
-        MailBasic mail = new MailBasic(tos, "测试", "我爱你");
-        mailService.send(mail);
+    public CommonResult search(@RequestBody @Validated({DefaultGroup.class}) UserDetailParam param) {
+        info("Account registration interface input parameters {}",
+            param.getUserName()); // 账号体中绝大部分信息属于敏感信息，不打印入参的其他参数的日志
+        int select = userInfoDao.select();
+        info(Integer.toString(select));
         return CommonResult.success();
     }
 }
--- account/src/main/java/vip/vinyoung/account/dao/UserInfoDao.java
+++ account/src/main/java/vip/vinyoung/account/dao/UserInfoDao.java
@@ -1,8 +1,8 @@
 package vip.vinyoung.account.dao;
 
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.Mapper;
 
-@Repository
+@Mapper
 public interface UserInfoDao {
     int select();
 }
